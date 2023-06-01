a8f784436bb0929ac508ac4220df6f2858283665
Gabriel <ougabriel@gmail.com>
2023-06-01 02:02:06 +0100

    Added multiple azure terraform files

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  resource_group="app-grp"
+  location="North Europe"  
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ARM_SUBSCRIPTION_ID" {
+    description = "The Azure Subscription ID"
+    type = string
+}
+
+variable "ARM_CLIENT_ID" {
+    description = "The Azure Service Principal Application ID"
+    type = string
+}
+variable "ARM_CLIENT_SECRET" {
+    description = "The Password for the Service Principal"
+    type = string
+}
+variable "ARM_TENANT_ID" {
+    description = "The Azure Tenant ID"
+    type = string
+}
